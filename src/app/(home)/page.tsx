b36f938bbbdbeb8994648007a34b01ee54e194e2
Kedves <93329472+Kedves1@users.noreply.github.com>
2024-10-25 09:36:16 +0200

    Feat: homepage info hidden

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,4 +1,7 @@
 import Info from "@/app/(home)/components/Info";
+import { cn } from "@/lib/utils";
+
+const showInfo = false;
 
 export default function Home() {
   return (
@@ -14,16 +17,22 @@ export default function Home() {
     //   </div>
     // </div>
 
-    (<div className="w-full pt-[100px] pb-10 h-screen px-10 flex justify-between max-md:flex-col">
-      <div className="text-[7rem] h-full items-end flex max-md:text-7xl leading-none max-md:hidden  font-black">
+    <div className="w-full pt-[100px] pb-10 h-screen px-10 flex justify-between max-md:flex-col">
+      <div
+        className={cn(
+          "text-[7rem] h-full items-end flex max-md:text-7xl leading-none font-black",
+          { "max-md:hidden": showInfo }
+        )}
+      >
         Pollák <br /> E-sport
       </div>
       <div className="h-full flex justify-between flex-col">
-        <Info />
+        {showInfo ? <Info /> : <div></div>}
+
         <div className="text-5xl w-full justify-end flex italic max-md:text-3xl ">
           2024.11.1 - 2024.11.10
         </div>
       </div>
-    </div>)
+    </div>
   );
 }
