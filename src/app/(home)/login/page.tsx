a2365988bbf44374c6d5d4b8d482984ef368089e
TGyAkos <85934789+TGyAkos@users.noreply.github.com>
2024-10-28 21:38:42 +0100

    added Keycloak login view, with necessary callback route

--- src/app/(home)/login/page.tsx
+++ src/app/(home)/login/page.tsx
@@ -1,112 +1,145 @@
-  "use client";
-  import React, { useState, useEffect } from "react";
-  import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-  } from "@/components/ui/card";
-  import { Button } from "@/components/ui/button";
-  import { toast } from "sonner";
-  import { Input } from "@/components/ui/input";
-  import { Label } from "@/components/ui/label";
-  import Link from "next/link";
-  import { Skeleton } from "@/components/ui/skeleton";
+"use client";
+import React, {useState, useEffect, FormEvent} from "react";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import Link from "next/link";
+import { Skeleton } from "@/components/ui/skeleton";
+import { signIn } from "next-auth/react";
 
-const Login = () => {
-    const [loading, setLoading] = useState(true);
+
+export default function Login() {
+    const [loading, setLoading] = useState(false);
 
     useEffect(() => {
       const timer = setTimeout(() => setLoading(false), 2000); // Simulate loading time
       return () => clearTimeout(timer);
     }, []);
 
-    const handleLoginClick = () => {
-      setLoading(true);
-      setTimeout(() => {
-        setLoading(false);
-        toast("Sikeres bejelentkezés", {
-          description: new Date().toLocaleTimeString([], {
-            hour: "2-digit",
-            minute: "2-digit",
-          }),
+    // This can ONLY work from the server side
+    const onSubmit = async (event: FormEvent<HTMLFormElement>) => {
+      setLoading(false);
+      event.preventDefault();
+
+      const formData = new FormData(event.currentTarget);
+
+      console.log(formData.get("username"));
+      console.log(formData.get("password"));
+
+      // TODO add proper form validation
+      if (formData.get("username") == "" || formData.get("password") == "") {
+        toast("Hiba történt a bejelentkezés során", {
+          description: "Kérlek próbáld újra!",
           action: {
             label: "Törlés",
             onClick: () => console.log("Értesítés törölve!"),
           },
         });
-      }, 1000); // Simulate login process
+        return
+      }
+
+      await signIn("credentials", {
+        username: formData.get("username"),
+        password: formData.get("password"),
+        redirect: false,
+      });
+
+      setLoading(false);
+
+      toast("Sikeres bejelentkezés", {
+        description: new Date().toLocaleTimeString([], {
+          hour: "2-digit",
+          minute: "2-digit",
+        }),
+        action: {
+          label: "Törlés",
+          onClick: () => console.log("Értesítés törölve!"),
+        },
+      });
     };
 
     return (
-      <div className="flex justify-center items-center min-h-screen">
-        <Card className="w-[350px]">
-          <CardHeader>
-            {loading ? (
-              <>
-                <Skeleton className="h-6 w-1/2 mb-2" />
-                <Skeleton className="h-4 w-3/4" />
-              </>
-            ) : (
-              <>
-                <CardTitle>Bejelentkezés</CardTitle>
-                <CardDescription>A fiókodba itt tudsz belépni.</CardDescription>
-              </>
-            )}
-          </CardHeader>
-          <CardContent>
-            {loading ? (
+    <div className="flex justify-center items-center min-h-screen">
+      <Card className="w-[350px]">
+        <CardHeader>
+          {loading ? (
+            <>
+              <Skeleton className="h-6 w-1/2 mb-2" />
+              <Skeleton className="h-4 w-3/4" />
+            </>
+          ) : (
+            <>
+              <CardTitle>Bejelentkezés</CardTitle>
+              <CardDescription>A fiókodba itt tudsz belépni.</CardDescription>
+            </>
+          )}
+        </CardHeader>
+        <CardContent>
+          {loading ? (
+            <div className="grid w-full items-center gap-4">
+              <Skeleton className="h-10 w-full mb-4" />
+              <Skeleton className="h-10 w-full" />
+            </div>
+          ) : (
+            <form
+              action="/api/auth/callback/credentials"
+              onSubmit={onSubmit}
+              method={"post"}
+            >
               <div className="grid w-full items-center gap-4">
-                <Skeleton className="h-10 w-full mb-4" />
-                <Skeleton className="h-10 w-full" />
-              </div>
-            ) : (
-              <form>
-                <div className="grid w-full items-center gap-4">
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="username">Felhasználónév</Label>
-                    <Input
-                      required
-                      id="username"
-                      type="text"
-                      placeholder="kisjanos88"
-                    />
-                  </div>
-                  <div className="flex flex-col space-y-1.5">
-                    <Label htmlFor="password">Jelszó</Label>
-                    <Input
-                      required
-                      id="password"
-                      type="password"
-                      placeholder="************"
-                    />
-                  </div>
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="username">Felhasználónév</Label>
+                  <Input
+                    required
+                    id="username"
+                    name="username"
+                    type="text"
+                    placeholder="kisjanos88"
+                  />
                 </div>
-              </form>
-            )}
-          </CardContent>
-          <CardFooter className="flex justify-between">
-            {loading ? (
-              <>
-                <Skeleton className="h-10 w-[calc(40%-0.5rem)]" />
-                <Skeleton className="h-10 w-[calc(30%-0.5rem)]" />
-              </>
-            ) : (
-              <>
-                <Button variant="outline">
-                  <Link href="/register">Regisztráció</Link>
-                </Button>
-                <Button type="button" onClick={handleLoginClick}>
-                  Belépés
-                </Button>
-              </>
-            )}
-          </CardFooter>
-        </Card>
-      </div>
-    );
-  }
+                <div className="flex flex-col space-y-1.5">
+                  <Label htmlFor="password">Jelszó</Label>
+                  <Input
+                    required
+                    id="password"
+                    name="password"
+                    type="password"
+                    placeholder="************"
+                  />
+                </div>
+              </div>
+              {loading ? (
+                  <>
+                    <Skeleton className="h-10 w-[calc(40%-0.5rem)]" />
+                    <Skeleton className="h-10 w-[calc(30%-0.5rem)]" />
+                  </>
+              ) : (
+                  <>
+                    <Button variant="outline">
+                      <Link href="/register">Regisztráció</Link>
+                    </Button>
+                    <Button type="submit">
+                      Belépés
+                    </Button>
+                  </>
+              )}
+            </form>
+          )}
+        </CardContent>
+        <CardFooter className="flex justify-between">
 
-  export default Login;
+        </CardFooter>
+      </Card>
+    </div>
+  );
+}
+
