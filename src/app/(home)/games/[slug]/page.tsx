bb3f4b4b8be4424cd21912c3dfb6f3f6a981e312
Kedves1 <gombarmartin2006@gmail.com>
2024-11-04 13:47:40 +0100

    Feat: Bracket style

--- src/app/(home)/games/[slug]/page.tsx
+++ src/app/(home)/games/[slug]/page.tsx
@@ -10,8 +10,9 @@ import {
   IRenderSeedProps,
 } from "react-brackets";
 import { Skeleton } from "@/components/ui/skeleton";
-const BracketsPage = async ({ params }: { params: Promise<{ slug: string }> }) => {
-  const { slug } = await params;
+import { useParams } from "next/navigation";
+const BracketsPage = () => {
+  const params = useParams<{ tag: string; item: string }>();
   const [loading, setLoading] = useState(true);
   const [rounds, setRounds] = useState<IRoundProps[]>([]);
 
@@ -27,7 +28,10 @@ const BracketsPage = async ({ params }: { params: Promise<{ slug: string }> }) =
               date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
                 hour12: false,
               }),
-              teams: [{ name: "Team B" }, { name: "Team V" }],
+              teams: [
+                { name: "Team B", score: 3 },
+                { name: "Team V", score: 1 },
+              ],
             },
             {
               id: 3,
@@ -85,11 +89,11 @@ const BracketsPage = async ({ params }: { params: Promise<{ slug: string }> }) =
         },
       ]);
       setLoading(false);
-    }, 2000); // Simulate a 2-second loading time
+    }, 0); // Simulate a 2-second loading time
   }, []);
 
   return (
-    <div className="w-fit overflow-x-scroll h-full flex gap-20 pt-[50px] flex-col px-32 mt-[100px]">
+    <div className="w-full h-full flex pt-[150px] flex-col px-32">
       <div className="border-b-[1px] border-b-white/20 w-full text-3xl pb-5">
         Ranbow Six Seige - 1v1 - 2024.11.1
       </div>
@@ -97,9 +101,10 @@ const BracketsPage = async ({ params }: { params: Promise<{ slug: string }> }) =
         <Skeleton className="w-full h-96" />
       ) : (
         <Bracket
+          bracketClassName="flex justify-center"
           roundTitleComponent={(title: React.ReactNode, roundIndex: number) => {
             return (
-              <div className="text-center bg-slate-900 p-2 mx-2 mb-14">
+              <div className="text-center bg-slate-900 p-2 border-r-2 border-black mb-14">
                 {title}
               </div>
             );
@@ -120,27 +125,37 @@ const CustomSeed = ({
 }: IRenderSeedProps) => {
   return (
     <Seed mobileBreakpoint={breakpoint}>
-      <SeedItem>
-        <div>
-          <SeedTeam>
-            <Image
-              src={"https://placehold.co/25x25"}
-              alt={"team logo"}
-              width={25}
-              height={25}
-              className="rounded-full"
-            />
-            {seed.teams[0]?.name || "NO TEAM "}
+      <SeedItem className="!rounded-xl">
+        <div className="">
+          <SeedTeam className="!py-0 !px-0 h-full">
+            <div className="flex flex-shrink-0 gap-2">
+              <div className="px-2 py2">{seed.teams[0]?.score}</div>
+              <Image
+                src={"https://placehold.co/25x25"}
+                alt={"team logo"}
+                width={25}
+                height={25}
+                className="rounded-full"
+              />
+              {seed.teams[0]?.name || "NO TEAM "}
+            </div>
+            <div className="bg-purple-900 h-full py-2 px-2 rounded-se-xl">
+              1
+            </div>
           </SeedTeam>
-          <SeedTeam className="text-white/20">
-            <Image
-              src={"https://placehold.co/25x25"}
-              alt={"team logo"}
-              width={25}
-              height={25}
-              className="rounded-full"
-            />
-            {seed.teams[1]?.name || "NO TEAM "}
+          <SeedTeam className="text-white/20 border-t-[1px] border-t-black !py-0 !px-0 h-full">
+            <div className="flex flex-shrink-0 gap-2">
+              <div className="px-2 py-2">{seed.teams[1]?.score}</div>
+              <Image
+                src={"https://placehold.co/25x25"}
+                alt={"team logo"}
+                width={25}
+                height={25}
+                className="rounded-full"
+              />
+              {seed.teams[1]?.name || "NO TEAM "}
+            </div>
+            <div className="px-2 py-2 bg-slate-600 rounded-ee-xl">0</div>
           </SeedTeam>
         </div>
       </SeedItem>
