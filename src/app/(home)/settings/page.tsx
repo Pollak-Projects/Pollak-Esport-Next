48067da4d406053ef4e7803557e2f6189d7f06a1
Kedves1 <gombarmartin2006@gmail.com>
2024-12-02 13:57:10 +0100

    Feat: account edit

--- src/app/(home)/settings/page.tsx
+++ src/app/(home)/settings/page.tsx
@@ -15,73 +15,60 @@ import logo from "@/tempimg/logo2.png";
 const Settings = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <Card className="w-5/6 h-4/5 mt-10">
+      <Card className="w-4/12 h-4/5 mt-10">
         <CardHeader>
-          <CardTitle className="text-center text-5xl">Beállítások</CardTitle>
+          <CardTitle className="text-left text-5xl">Felhasználó</CardTitle>
         </CardHeader>
-        <CardContent>
-          <div className="flex flex-row">
-            <div className="flex flex-wrap flex-col space-y-4 w-48">
-              <div className="flex flex-wrap flex-col space-y-4 w-48">
-                <Button variant={"outline"} className="self-start w-48">
-                  Fiók
-                </Button>
-                <Button variant={"outline"} className="self-start w-48">
-                  Button 2
-                </Button>
-                <Button variant={"outline"} className="self-start w-48">
-                  Button 3
-                </Button>
-                <Button variant={"outline"} className="self-start w-48">
-                  Button 4
-                </Button>
-                <Button variant={"outline"} className="self-start w-48">
-                  Button 5
-                </Button>
-                <Button variant={"outline"} className="self-start w-48">
-                  Fiók törlése
-                </Button>
-              </div>
+        <CardContent className="w-full">
+          <div className="flex flex-col-reverse w-full">
+            <div className="flex justify-end w-full my-10">
+              <Button
+                variant={"outline"}
+                className="self-start w-48 border-red-600 !text-red-600"
+              >
+                Fiók törlése
+              </Button>
             </div>
-            <div className="flex-1 ml-4 md:ml-16 pt-4 md:pt-10 pl-4 md:pl-10 border border-black rounded-lg bg-slate-900 overflow-y-auto">
-              <div className="flex flex-col md:flex-row md:space-x-16 space-y-4 md:space-y-0 items-start md:items-center">
-                <div className="flex flex-col space-y-4 w-full md:w-auto">
-                  <Label htmlFor="lastname">Vezetéknév</Label>
-                  <Input
-                    id="lastname"
-                    className="w-full max-w-[12rem]"
-                    defaultValue={"Kis"}
-                  />
+            <div className="flex-1 pt-4 pl-4 md:pl-10 border border-black rounded-lg bg-slate-900 overflow-y-auto">
+              <div className="flex flex-col-reverse md:space-x-16 space-y-4 md:space-y-0">
+                <div className="flex gap-2 mr-10">
+                  <div className="flex flex-col space-y-4 w-full md:w-auto">
+                    <Label htmlFor="lastname">Vezetéknév</Label>
+                    <Input
+                      id="lastname"
+                      className="w-full max-w-[12rem]"
+                      defaultValue={"Kis"}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-4 w-full md:w-auto">
+                    <Label htmlFor="firstname">Keresztnév</Label>
+                    <Input
+                      id="firstname"
+                      className="w-full max-w-[12rem]"
+                      defaultValue={"János"}
+                    />
+                  </div>
+                  <div className="flex flex-col space-y-4 w-full md:w-auto">
+                    <Label htmlFor="username">Felhasználónév</Label>
+                    <Input
+                      id="username"
+                      className="w-full max-w-[12rem]"
+                      defaultValue={"kisjanos88"}
+                    />
+                  </div>
                 </div>
-                <div className="flex flex-col space-y-4 w-full md:w-auto">
-                  <Label htmlFor="firstname">Keresztnév</Label>
-                  <Input
-                    id="firstname"
-                    className="w-full max-w-[12rem]"
-                    defaultValue={"János"}
-                  />
-                </div>
-                <div className="flex flex-col space-y-4 w-full md:w-auto">
-                  <Label htmlFor="username">Felhasználónév</Label>
-                  <Input
-                    id="username"
-                    className="w-full max-w-[12rem]"
-                    defaultValue={"kisjanos88"}
-                  />
-                </div>
-                <div className="md:ml-4">
-                  <Label className="mb-4 block">Profilkép</Label>
-                  <div>
+                <div className="!ml-0 flex w-ful flex-col !mb-[60px]">
+                  <div className="m-auto">
                     <div
-                      className="w-16 h-16 rounded-full cursor-pointer overflow-hidden"
+                      className="rounded-full cursor-pointer overflow-hidden"
                       onClick={() => document.getElementById("avatar")?.click()}
                     >
                       <Image
                         src={logo}
                         alt="Profile"
-                        width={96}
-                        height={96}
-                        className="w-full h-full object-cover"
+                        width={200}
+                        height={200}
+                        className="hover:brightness-50 transition"
                       />
                     </div>
                     <input
@@ -98,9 +85,10 @@ const Settings = () => {
                       }}
                     />
                   </div>
+                  <Label className="mb-4 block text-center">Profilkép</Label>
                 </div>
               </div>
-              <div className="flex justify-center space-x-4 w-full mt-8 md:mt-28">
+              <div className="flex justify-end py-5 px-10 space-x-4 w-full mt-8 md:mt-28">
                 <Button variant="outline">Visszaállítás</Button>
                 <Button>Mentés</Button>
               </div>
