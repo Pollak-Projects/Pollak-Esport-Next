db749c93cfc19af8a287fd14d2f9bc9d7f146a36
Kedves <gombarmartin2006@gmail.com>
2025-01-19 20:03:47 +0100

    Feat: API bracket & status

--- src/app/(home)/games/[slug]/page.tsx
+++ src/app/(home)/games/[slug]/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import Image from "next/image";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import {
   Bracket,
   IRoundProps,
@@ -9,186 +9,142 @@ import {
   SeedTeam,
   IRenderSeedProps,
 } from "react-brackets";
-import { Skeleton } from "@/components/ui/skeleton";
 import { useParams } from "next/navigation";
-import { team } from "@/app";
-import TeamCard from "../../components/TeamCard";
-const BracketsPage = () => {
-  const params = useParams<{ tag: string; item: string }>();
-  const [loading, setLoading] = useState(true);
-  const [rounds, setRounds] = useState<IRoundProps[]>([]);
+// import { team } from "@/app";
+// import TeamCard from "../../components/TeamCard";
+import { useQuery } from "@tanstack/react-query";
+import Spinner from "@/components/Spinner";
+import { Circle } from "lucide-react";
 
-  useEffect(() => {
-    // Simulate data fetching
-    setTimeout(() => {
-      setRounds([
-        {
-          title: "Negyeddöntő",
-          seeds: [
-            {
-              id: 1,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team B", score: 3 },
-                { name: "Team V", score: 1 },
-              ],
-            },
-            {
-              id: 3,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team A", score: 3 },
-                { name: "Team F", score: 1 },
-              ],
-            },
-            {
-              id: 2,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team C", score: 3 },
-                { name: "Team G", score: 1 },
-              ],
-            },
-            {
-              id: 4,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team D", score: 3 },
-                { name: "Team E", score: 1 },
-              ],
-            },
-          ],
+const BracketsPage = () => {
+  const params = useParams<{
+    slug: any;
+    tag: string;
+    item: string;
+  }>();
+  const currentId = params.slug;
+
+  const getRounds = async () => {
+    const res = await fetch(
+      `https://esportbackend.gemes.eu/game/${currentId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
         },
-        {
-          title: "Elődöntő",
-          seeds: [
-            {
-              id: 1,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team A", score: 3 },
-                { name: "Team B", score: 1 },
-              ],
-            },
-            {
-              id: 2,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team C", score: 3 },
-                { name: "Team D", score: 1 },
-              ],
-            },
-          ],
-        },
-        {
-          title: "Döntő",
-          seeds: [
-            {
-              id: 3,
-              date: new Date(2024, 11, 20, 3, 0, 0).toLocaleString("hu-HU", {
-                hour12: false,
-              }),
-              teams: [
-                { name: "Team A", score: 3 },
-                { name: "Team C", score: 1 },
-              ],
-            },
-          ],
-        },
-      ]);
-      setLoading(false);
-    }, 1000); // Simulate a 2-second loading time
-  }, []);
-  const teams: team[] = [
-    {
-      name: "jaa",
-      image: "https://placehold.co/125x125",
-      competition: "R6 2v2",
-      players: [
-        {
-          name: "Árpi",
-          image: "https://placehold.co/75x75",
-          isTeamLeader: true,
-        },
-        {
-          name: "Feri",
-          image: "https://placehold.co/75x75",
-          isTeamLeader: false,
-        },
-      ],
-    },
-    {
-      name: "das",
-      image: "https://placehold.co/125x125",
-      competition: "R6 2v2",
-      players: [
-        {
-          name: "Dominik",
-          image: "https://placehold.co/75x75",
-          isTeamLeader: true,
-        },
-        {
-          name: "Dániel",
-          image: "https://placehold.co/75x75",
-          isTeamLeader: false,
-        },
-      ],
-    },
-  ];
+      }
+    );
+
+    return res.json();
+  };
+
+  const { data, error, isLoading } = useQuery({
+    queryKey: ["games", currentId],
+    queryFn: getRounds,
+  });
+
+  // const teams: team[] = [
+  //   {
+  //     name: "jaa",
+  //     image: "https://placehold.co/125x125",
+  //     competition: "R6 2v2",
+  //     players: [
+  //       {
+  //         name: "Árpi",
+  //         image: "https://placehold.co/75x75",
+  //         isTeamLeader: true,
+  //       },
+  //       {
+  //         name: "Feri",
+  //         image: "https://placehold.co/75x75",
+  //         isTeamLeader: false,
+  //       },
+  //     ],
+  //   },
+  //   {
+  //     name: "das",
+  //     image: "https://placehold.co/125x125",
+  //     competition: "R6 2v2",
+  //     players: [
+  //       {
+  //         name: "Dominik",
+  //         image: "https://placehold.co/75x75",
+  //         isTeamLeader: true,
+  //       },
+  //       {
+  //         name: "Dániel",
+  //         image: "https://placehold.co/75x75",
+  //         isTeamLeader: false,
+  //       },
+  //     ],
+  //   },
+  // ];
+
+  if (isLoading) {
+    return <Spinner />;
+  }
+
+  const rounds: IRoundProps[] = [];
+  data.data[0].bracket
+    .replaceAll(`\\`, "")
+    .split(";")
+    .map((round: string) => {
+      rounds.push(JSON.parse(round));
+    });
 
   return (
     <div className="h-screen flex flex-col">
       <div className="w-full max-md:w-fit flex pt-[150px] flex-col md:px-32 overflow-x-scroll">
-        <div className="border-b-[1px] whitespace-nowrap w-full border-b-white/20 text-3xl pb-2">
-          Ranbow Six Seige - 1v1 - 2024.11.1
-        </div>
-        {loading ? (
-          <Skeleton className="w-full h-96" />
-        ) : (
-          <Bracket
-            bracketClassName="flex justify-center w-fit pb-10"
-            roundTitleComponent={(
-              title: React.ReactNode,
-              roundIndex: number
-            ) => {
-              return (
-                <div className="text-center bg-slate-900 p-2 border-r-2 border-black mb-14">
-                  {title}
-                </div>
-              );
-            }}
-            rounds={rounds}
-            mobileBreakpoint={0}
-            renderSeedComponent={CustomSeed}
-          />
-        )}
-      </div>
-      <div className="w-full flex flex-grow bg-black px-20 justify-between">
-        <div className="">
-          <div className="text-5xl">Leírás</div>
-          <div className="text-slate-300">
-            Blaa blaah blahh vmi játlk ebmerekkel és csapatokkal Lorem ipsum,
-            dolor sit amet consectetur adipisicing elit. Iste, qui?
+        <div className="border-b-[1px] whitespace-nowrap w-full border-b-white/20 text-3xl flex justify-between items-center">
+          <div className="flex gap-10 items-center">
+            <div className="flex justify-start gap-2 items-center bg-slate-900 py-2 px-5">
+              <Circle
+                fill={
+                  data.data[0].status === "Hamarosan"
+                    ? "gray"
+                    : data.data[0].status === "Vége"
+                    ? "red"
+                    : "green"
+                }
+                size={15}
+                color={
+                  data.data[0].status === "Hamarosan"
+                    ? "gray"
+                    : data.data[0].status === "Vége"
+                    ? "red"
+                    : "green"
+                }
+              ></Circle>
+              {data.data[0].status}
+            </div>
+            <div className="">
+              {data.data[0].name +
+                " - " +
+                data.data[0].playerPerTeam +
+                "v" +
+                data.data[0].playerPerTeam}
+            </div>
+          </div>
+          <div className="">
+            {data.data[0].startDate.split("T")[0].replaceAll("-", ".") +
+              " - " +
+              data.data[0].endDate.split("T")[0].replaceAll("-", ".")}
           </div>
         </div>
-        <div className="">
-          {teams.map((team) => {
-            return <TeamCard team={team} key={team.name} />;
-          })}
-        </div>
+        <Bracket
+          bracketClassName="flex justify-center w-fit pb-10"
+          roundTitleComponent={(title: React.ReactNode, roundIndex: number) => {
+            return (
+              <div className="text-center bg-slate-900 p-2 border-r-2 border-black mb-14">
+                {title}
+              </div>
+            );
+          }}
+          rounds={rounds}
+          mobileBreakpoint={0}
+          renderSeedComponent={CustomSeed}
+        />
       </div>
+      <div className="w-full flex flex-grow bg-black/30 px-20 justify-between"></div>
     </div>
   );
 };
@@ -229,7 +185,7 @@ const CustomSeed = ({ seed }: IRenderSeedProps) => {
             <div className="bg-slate-600 h-full py-2 px-2 rounded-ee-xl">0</div>
           </SeedTeam>
           <div className="text-[10px] mt-1 text-slate-400 absolute w-full">
-            {seed.date}
+            {seed.date?.replace("T", " ").replaceAll("-", ".")}
           </div>
         </div>
       </SeedItem>
--- src/app/(home)/games/page.tsx
+++ src/app/(home)/games/page.tsx
@@ -9,8 +9,10 @@ import {
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
-import { Skeleton } from "@/components/ui/skeleton";
 import { useQuery } from "@tanstack/react-query";
+import Spinner from "@/components/Spinner";
+import { cn } from "@/lib/utils";
+import { Circle } from "lucide-react";
 
 const getGames = async () => {
   const res = await fetch(`https://esportbackend.gemes.eu/game`, {
@@ -21,39 +23,16 @@ const getGames = async () => {
 
   return res.json();
 };
+
 const Games = () => {
+  const [hover, setHover] = React.useState<number>();
   const { data, error, isLoading } = useQuery({
     queryKey: ["games"],
     queryFn: getGames,
   });
 
   if (isLoading) {
-    return (
-      <div className="grid pt-[140px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-4 mx-auto max-w-[1400px] ">
-        {Array.from({ length: 4 }).map((_, i) => (
-          <Card
-            key={i}
-            className="shadow-md shadow-purple-900 w-[400px] border-b-purple-900 border-b-4 bg-black/20"
-          >
-            <div className="relative w-full h-[130px]">
-              <Skeleton className="w-full h-full" />
-            </div>
-            <CardHeader>
-              <Skeleton className="h-6 w-3/4" />
-            </CardHeader>
-            <CardContent>
-              <Skeleton className="h-4 w-full" />
-              <Skeleton className="h-4 w-5/6 mt-2" />
-              <Skeleton className="h-4 w-2/3 mt-2" />
-            </CardContent>
-            <CardFooter className="flex justify-between items-center">
-              <Skeleton className="h-4 w-1/3" />
-              <Skeleton className="h-4 w-1/4" />
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
-    );
+    return <Spinner />;
   }
 
   return (
@@ -61,7 +40,34 @@ const Games = () => {
       <div className="grid pt-[140px] grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-4 p-4 mx-auto max-w-[1400px] ">
         {data.data.map((game: any, i: number) => (
           <Link key={game.name} href={`/games/${game.id}`}>
-            <Card className="shadow-md shadow-purple-900 border-[0px] w-[400px] hover:scale-105 duration-300 transition-all ease-in-out hover:shadow-2xl  hover:shadow-purple-900 border-b-purple-900 border-b-4 bg-black/[0] backdrop-blur-xl ">
+            <Card
+              className={cn(
+                "shadow-md relative shadow-purple-900 border-[0px] w-[400px] hover:scale-105 duration-300 transition-all ease-in-out hover:shadow-2xl  hover:shadow-purple-900 border-b-purple-900 border-b-4 bg-black/20",
+                { "opacity-5": hover !== game.id && hover != undefined }
+              )}
+              onMouseEnter={() => setHover(game.id)}
+              onMouseLeave={() => setHover(undefined)}
+            >
+              <div className="w-full h-[30px] absolute z-30 top-2 flex justify-start px-4 gap-2 items-center">
+                <Circle
+                  fill={
+                    game.status === "Hamarosan"
+                      ? "gray"
+                      : game.status === "Vége"
+                      ? "red"
+                      : "green"
+                  }
+                  size={15}
+                  color={
+                    game.status === "Hamarosan"
+                      ? "gray"
+                      : game.status === "Vége"
+                      ? "red"
+                      : "green"
+                  }
+                ></Circle>
+                {game.status}
+              </div>
               <div className="relative w-full h-[130px]">
                 <Image
                   src={game.image}
@@ -71,26 +77,30 @@ const Games = () => {
                 />
               </div>
               <CardHeader>
-                <CardTitle>{game.name}</CardTitle>
+                <CardTitle>
+                  {game.name +
+                    " - " +
+                    game.playerPerTeam +
+                    "v" +
+                    game.playerPerTeam}
+                </CardTitle>
               </CardHeader>
               <CardContent>
-                <p>{game.description || "nincs leírás"}</p>
-              </CardContent>
-              <CardFooter className="flex justify-between items-center">
-                <p>
-                  {game.startDate
-                    .split("T")[0]
-                    .replace("-", ".")
-                    .replace("-", ".")}{" "}
-                  -{" "}
-                  {game.endDate
-                    .split("T")[0]
-                    .replace("-", ".")
-                    .replace("-", ".")}
+                <p className="text-slate-300">
+                  {game.description || "nincs leírás"}
                 </p>
-                <div className="italic">
-                  További információk <span className=" font-black">{"⭢"}</span>
-                </div>
+              </CardContent>
+              <CardFooter className="flex justify-between items-center mt-5">
+                <p>
+                  {game.startDate.split("T")[0].replaceAll("-", ".")} -{" "}
+                  {game.endDate.split("T")[0].replaceAll("-", ".")}
+                </p>
+                {game.status !== "Hamarosan" && (
+                  <div className="italic">
+                    További információk{" "}
+                    <span className=" font-black">{"⭢"}</span>
+                  </div>
+                )}
               </CardFooter>
             </Card>
           </Link>
